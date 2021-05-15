96c959d0ff0f3f383b84975a4b2873f9544b88db
yassar123 <yassarali1999@gmail.com>
2021-05-16 00:30:02 +0530

    component added

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="" />
+    <Welcome name="yassar" />
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+import React from "react";
+function Welcome(){
+    return(
+        <>
+        <h1>Hey !</h1>
+        <h2>Welcome to Newton School.</h2>
+        </>
+    )
+}
+export default Welcome
